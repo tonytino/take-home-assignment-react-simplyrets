f3902ace12c7449b71561ee609581028282ccfbd
Anthony Hernandez <tony91139@gmail.com>
2024-01-15 18:42:58 -0700

    feat(<PropertyListing /): Create and partially implement

---
+++ src/components/PropertyListing/PropertyListing.jsx
@@ -0,0 +1,39 @@
+import heartFill from 'assets/heart-fill.svg';
+import heartStroke from 'assets/heart-stroke.svg';
+import styles from './PropertyListing.module.css';
+
+/**
+ * Renders a property listing's details, which can be favorited
+ * @param {Object} props
+ */
+export function PropertyListing(props) {
+  const { isFavorited = false, listing = {}, onFavorite = () => {} } = props;
+  const { photos = [] } = listing;
+  const photoSrc = photos[0];
+
+  const favoriteToggleButtonLabel = `${isFavorited ? 'Remove from' : 'Add to'} your favorites`;
+
+  return (
+    <div className={styles.PropertyListing}>
+      <button
+        aria-label={favoriteToggleButtonLabel}
+        className={styles.FavoriteToggle}
+        onClick={onFavorite}
+      >
+        {isFavorited ? (
+          <img alt="Favorite icon" height={48} src={heartFill} width={48} />
+        ) : (
+          <img alt="Unfavorite icon" height={48} src={heartStroke} width={48} />
+        )}
+      </button>
+
+      <img
+        alt={'TODO'}
+        className={styles.Image}
+        height={240}
+        src={photoSrc}
+        width={320}
+      />
+    </div>
+  );
+}
--- src/components/PropertyListing/index.js
+++ src/components/PropertyListing/index.js
@@ -1,2 +1,1 @@
-export { Layout } from './Layout';
 export { PropertyListing } from './PropertyListing';
