9ca13e13bee35bbe4973888061da5551876c0923
Anthony Hernandez <tony91139@gmail.com>
2024-01-15 18:07:16 -0700

    feat(<Layout />): Present page name in header and document.title

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,27 @@
 import * as React from 'react';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import styles from './Layout.module.css';
 
+const PAGE_NAMES = {
+  '/': 'Side',
+  '/property-listings': 'Property Listings',
+};
+
 /**
- * Renders the page wrapped in the web app's layout
+ * Renders the page wrapped in the web app's layout while also updating the document.title
  */
 export function Layout() {
+  const location = useLocation();
+  const pageName = PAGE_NAMES[location?.pathname] ?? 'Side';
+
+  React.useEffect(() => {
+    document.title = pageName;
+  }, [pageName]);
+
   return (
     <div className={styles.Layout}>
       <header className={styles.Header}>
-        <h1 className={styles.PageName}>TODO</h1>
+        <h1 className={styles.PageName}>{pageName}</h1>
       </header>
 
       <main className={styles.PageContents}>
